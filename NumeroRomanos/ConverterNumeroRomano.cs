5e08825cb9de66e1dba588ea3da7eb1ab65c5762
Angelo RB <Angelo-RB>
2021-06-10 17:56:22 -0300

    Add project files.

---
+++ NumeroRomanos/ConverterNumeroRomano.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorRomano.ConsoleApp
+{
+    class ConverterNumeroRomano
+    {
+        public int ConverterNumeroRomanoSecundario()
+        {
+            char[] romano = { 'x' };
+
+            Dictionary<char, short> numeraisRomanos = new Dictionary<char, short>();
+            numeraisRomanos.Add('M', 1000);
+            numeraisRomanos.Add('C', 100);
+            numeraisRomanos.Add('L', 50);
+            numeraisRomanos.Add('X', 10);
+            numeraisRomanos.Add('V', 5);
+            numeraisRomanos.Add('I', 1);
+            numeraisRomanos.Add('m', 1000);
+            numeraisRomanos.Add('c', 100);
+            numeraisRomanos.Add('l', 50);
+            numeraisRomanos.Add('x', 10);
+            numeraisRomanos.Add('v', 5);
+            numeraisRomanos.Add('i', 1);
+
+            int arabico = 0;
+
+            for (int i = 0; i < romano.Count(); i++)
+            {
+
+                if (!numeraisRomanos.ContainsKey(romano[i]))
+                    return 0;
+
+                if (i == romano.Count() - 1)
+                {
+                    arabico += numeraisRomanos[romano[i]];
+                }
+                else
+                {
+                    if (numeraisRomanos[romano[i + 1]] > numeraisRomanos[romano[i]]) arabico -= numeraisRomanos[romano[i]];
+                    else arabico += numeraisRomanos[romano[i]];
+                }
+            }
+            return arabico;
+        }
+    }
+}
---
+++ NumeroRomanos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorRomano.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor Números Romanos");
+
+            ConversorNumeroIndoArabico conversorIndoArabico = new ConversorNumeroIndoArabico();
+
+            Console.WriteLine(conversorIndoArabico.ConversorNumeroIndoArabicoPrimario(3 + 3));
+            Console.ResetColor();
+
+            ConverterNumeroRomano conversorRomano = new ConverterNumeroRomano();
+
+            Console.WriteLine(conversorRomano.ConverterNumeroRomanoSecundario());
+            Console.ResetColor();
+
+            Console.ReadLine();
+        }
+    }
+}
+
